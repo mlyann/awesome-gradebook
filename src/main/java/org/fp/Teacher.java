ea943e2ade051a7b312a8ca807b7eaf67f36422a
Jerry Yu <dwesry@hotmail.com>
2025-04-06 21:38:08 -0700

    Updated Teacher class

--- src/main/java/org/fp/Teacher.java
+++ src/main/java/org/fp/Teacher.java
@@ -1,7 +1,37 @@
 package org.fp;
 
-public class Teacher extends  Student {
-	public Teacher(String studentId, String firstName, String lastName, String email) {
-        super(studentId, firstName, lastName, email);
+public class Teacher {
+	private final String firstName;
+    private final String lastName;
+    private  final String teacherID;
+
+    public Teacher(String teacherID, String firstName, String lastName) {
+        if (teacherID == null || teacherID.isEmpty()){
+            throw new IllegalArgumentException("Teacher ID cannot be null or empty.");
+        }
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.teacherID = teacherID;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public String getTeacherID(){
+        return teacherID;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Teacher: %s (%s)", getFullName(), getTeacherID());
     }
 }
